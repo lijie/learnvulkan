a1ecc0bd701405fa71edff51390e75c4cd20fd46
lijie <eltshanli@gmail.com>
2023-08-14 01:10:48 +0800

    add vertex buffer demo

--- compile_shader.bat
+++ compile_shader.bat
@@ -1,2 +1,7 @@
 C:\VulkanSDK\1.3.250.1\Bin\glslc.exe src\01-drawing-a-triangle\01-shader.vert -o build\Debug\01-vert.spv
 C:\VulkanSDK\1.3.250.1\Bin\glslc.exe src\01-drawing-a-triangle\01-shader.frag -o build\Debug\01-frag.spv
+
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe src\02-vertex-buffer\02-shader.vert -o build\Debug\02-vert.spv
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe src\02-vertex-buffer\02-shader.frag -o build\Debug\02-frag.spv
+
+pause
