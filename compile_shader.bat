bbfe3d926a36f1ce1198e524969f9dc8439f120c
lijie <eltshanli@gmail.com>
2023-08-15 00:51:38 +0800

    start 03-uniform-buffer

--- compile_shader.bat
+++ compile_shader.bat
@@ -4,4 +4,7 @@ C:\VulkanSDK\1.3.250.1\Bin\glslc.exe src\01-drawing-a-triangle\01-shader.frag -o
 C:\VulkanSDK\1.3.250.1\Bin\glslc.exe src\02-vertex-buffer\02-shader.vert -o build\Debug\02-vert.spv
 C:\VulkanSDK\1.3.250.1\Bin\glslc.exe src\02-vertex-buffer\02-shader.frag -o build\Debug\02-frag.spv
 
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe src\03-uniform-buffer\03-shader.vert -o build\Debug\03-vert.spv
+C:\VulkanSDK\1.3.250.1\Bin\glslc.exe src\03-uniform-buffer\03-shader.frag -o build\Debug\03-frag.spv
+
 pause
